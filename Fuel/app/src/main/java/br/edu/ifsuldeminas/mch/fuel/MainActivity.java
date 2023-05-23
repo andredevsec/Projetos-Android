fb75ad666f1b59931bd59ebdf6ebd2343bc61bce
Andre Luiz de Lima <11002167663@campus.machado.mch>
2023-05-22 22:52:08 -0300

    finalizado Fuel

--- Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
+++ Fuel/app/src/main/java/br/edu/ifsuldeminas/mch/fuel/MainActivity.java
@@ -1,12 +1,19 @@
 package br.edu.ifsuldeminas.mch.fuel;
 
+import static br.edu.ifsuldeminas.mch.fuel.R.id.editTextAlertDialogGasStationId;
 import static br.edu.ifsuldeminas.mch.fuel.R.id.imageViewFuel;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         imageViewFuel= findViewById(R.id.imageViewFuel);
         imageViewShare= findViewById(R.id.imageViewShare);
         textViewMessage= findViewById(R.id.textViewMessage);
+
 
         escondeComponentes();
 
@@ -81,5 +89,51 @@ public class MainActivity extends AppCompatActivity {
         imageViewFuel.setVisibility(imageViewFuel.INVISIBLE);
         imageViewShare.setVisibility(imageViewShare.INVISIBLE);
         textViewMessage.setVisibility(textViewMessage.INVISIBLE);
+    }
+
+    public void shareCLick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Preços de qual posto");
+
+        LayoutInflater inflater = getLayoutInflater();
+        View alert = inflater.inflate(R.layout.alert_dialog_gas_station_view, null);
+
+        builder.setView(alert);
+
+        builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                EditText nomePosto = alert.findViewById(editTextAlertDialogGasStationId);
+
+                String nomePostoStr = nomePosto.getText().toString();
+                String stringEtanol = textInputEditTextEtanol.getText().toString();
+                String stringGas = textInputEditTextGas.getText().toString();
+                Double valorEtanol = Double.parseDouble(stringEtanol);
+                Double valorGas = Double.parseDouble(stringGas);
+                Double proporcao = valorEtanol/valorGas*100;
+
+                String mensagem = String.format("Preços no posto '%s'. Etanol R$%.2f. Gasolina R$%.2f. " +
+                        "Proporção %.1f%. Melhor usar '%s'.",
+                        nomePostoStr, valorEtanol, valorGas, proporcao, proporcao>70 ? "Gasolina" : "Etanol");
+
+                Intent intent =  new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, mensagem);
+                intent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(intent, "Compartilhar preços combustivel");
+                startActivity(shareIntent);
+
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
     }
 }
